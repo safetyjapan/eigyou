f30bacf6b4cf177c45ed2517382059fcddc4da7f
safetyjapan <xsakitan21x@gmail.com>
2026-05-15 12:05:32 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // ここにSupabaseの情報を入力してください
 // Supabase > Settings > API からコピーできます
 // ============================================================
-const SUPABASE_URL = 'https://xxxx.supabase.co.cvvtfkypiimlyokyexfsL';
+const SUPABASE_URL = 'https://cvvtfkypiimlyokyexfs.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2dnRma3lwaWltbHlva3lleGZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDU2MTksImV4cCI6MjA5NDM4MTYxOX0.jA4VvoBZQRu7c54sqZsuo6WTxT1Mdogd9DBOCbhv_BQ';
 
 // パスワード設定
